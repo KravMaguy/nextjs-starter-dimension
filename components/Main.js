17af93939f3b714ad3715107b2225f7d31872515
krav maguy <flex4lease@gmail.com>
2021-03-31 14:31:49 -0500

    added job details

--- components/Main.js
+++ components/Main.js
@@ -65,7 +65,7 @@ class Main extends React.Component {
             <dt>Tags</dt>
             <dd>Coding and Design</dd>
             <dt>Technologies</dt>
-            <dd>React, Maps, Firebase, Twilio, Sendgrid, Paypal</dd>
+            <dd>React, Maps, Express.Js, Firebase, Twilio, Sendgrid, Paypal</dd>
             <dt>Categories</dt>
             <dd>Front End Development, Back End Development, Logo Design</dd>
             <dt>Link</dt>
@@ -124,6 +124,175 @@ class Main extends React.Component {
               production apps.
             </dd>
           </dl>
+
+          <span className="image main">
+            <img
+              src="/static/images/listing-app-screenshot.jpg"
+              alt="Screenshot of a listing-app.com admin area, website for listing and leasing commercial real estate"
+            />
+          </span>
+          <h2>
+            A free listing platform for real estate for sale and for lease, any
+            user can sign up and list spaces.
+          </h2>
+          <dl>
+            <dt>Tags</dt>
+            <dd>Coding and Design</dd>
+            <dt>Technologies</dt>
+            <dd>PHP, MySQL, Javascript, Jquery</dd>
+            <dt>Categories</dt>
+            <dd>
+              Front End Development, Back End Development, Full Stack
+              Development
+            </dd>
+            <dt>Link</dt>
+            <dd>
+              <a href="https://www.listing-app.com" target="_blank">
+                Listing-app.com
+              </a>
+            </dd>
+
+            <dt>Buisness Solution</dt>
+            <dd>
+              <p>
+                Provide a free platform for anyone to easily list, and search
+                for real estate for sale and for lease.
+              </p>
+            </dd>
+            <dt>This Application fulfills the following user stories:</dt>
+            <dd>
+              <ul>
+                <li>
+                  A user can register for an account and login using email and
+                  password
+                </li>
+                <li>
+                  A user can register for an account and login using social
+                  authentication (Facebook, LinkedIn, Microsoft Outlook 365)
+                </li>
+                <li>
+                  A user can Create, Read, Update and delete his buildings
+                  through the User Dashboard
+                </li>
+                <li>
+                  A user can Create, Read, Update and delete units belonging to
+                  individual buidlings through the User Dashboard
+                </li>
+                <li>
+                  A user can Upload multiple images Belonging to a building
+                </li>
+                <li>
+                  A user can Upload multiple images Belonging to an individual
+                  Unit in a building that he has listed for lease
+                </li>
+                <li>
+                  A user can set a featured image for a building as well as a
+                  featured image for an individual unit
+                </li>
+                <li>
+                  There is a gallery for each building, as well as a gallery for
+                  each individual unit created from the uusers uploaded photos.
+                </li>
+                <li>
+                  I can search the map by location and the map will tell me how
+                  many buildings were found in that particular search area
+                </li>
+                <li>
+                  After a search the map will display the city borders within a
+                  polygon drawn upon the map
+                </li>
+                <li>
+                  A user has the ability to publicly share a link of a map that
+                  show only his listings
+                </li>
+                <li>I can get directions to any listed building</li>
+                <li>
+                  No one outside of the app sponsor can list any buildings
+                  within 5 miles of the main buisness location
+                </li>
+                <li>
+                  A superAdmin has the ability to delete a user and all his
+                  buildings and spaceplans
+                </li>
+                <li>
+                  If I am a first time user that has logged in to the user
+                  dashboard i will get a tour showing me how to use the admin
+                  dashboard
+                </li>
+              </ul>
+            </dd>
+            <dt>Improvements</dt>
+            <dd>
+              It is a fairly in depth application. Would be great to have some
+              funding for it in order to be able to work with other programmers
+              in order to organize, refactor the code and complete the project,
+              convert existing procedural php code to object oriented php.
+              Remove unused css. Run Webpack. Remove and consolidate repeated
+              code.
+            </dd>
+          </dl>
+
+          <span className="image main">
+            <img
+              src="/static/images/auth-0.jpg"
+              alt="Screenshot of a auth0 application using social login"
+            />
+          </span>
+          <h2>
+            Auth 0 todos fullstack MERN application- fullstack todo application
+            implementing social login, and auth0 as authentication API.
+          </h2>
+          <p>
+            This application is a combination of two seperate tutorials:
+            <br />
+            <a
+              href="https://www.youtube.com/watch?v=X__rLNfTsLg&ab_channel=WebDevJunkie"
+              target="_blank"
+            >
+              FullStack react todo tutorial
+            </a>{" "}
+            - Credit Cody Sibert
+            <br />
+            <a
+              href="https://www.youtube.com/watch?v=1rgeO_EbSGg&ab_channel=Auth0"
+              target="_blank"
+            >
+              Auth 0 basic app startup
+            </a>{" "}
+            - Credit Auth 0
+          </p>
+          <dl>
+            <dt>Tags</dt>
+            <dd>Coding</dd>
+            <dt>Technologies</dt>
+            <dd>MongoDb, React, Auth0, Express</dd>
+            <dt>Categories</dt>
+            <dd>Full Stack Development</dd>
+            <dt>Link</dt>
+            <dd>
+              <a
+                href="https://github.com/KravMaguy/auth0-react-sdk-video"
+                target="_blank"
+              >
+                React Auth0 todos
+              </a>
+            </dd>
+
+            <dt>Buisness Solution</dt>
+            <dd>
+              <p>
+                Learn to use Auth0 api by creating a full stack todo application
+              </p>
+            </dd>
+            <dt>This application fulfills the following user stories:</dt>
+            <dd>
+              <ul>
+                <li>A user can login using social login or email/password</li>
+                <li>A user can create read update and delete his todos</li>
+              </ul>
+            </dd>
+          </dl>
+
           <span className="image main">
             <img
               src="/static/images/Mapbox3D.jpg"
@@ -183,6 +352,56 @@ class Main extends React.Component {
 
           <span className="image main">
             <img
+              src="/static/images/wordpress-rest-api.jpg"
+              alt="wordpress rest api"
+            />
+          </span>
+          <h2>Wordpress & Firebase Rest API</h2>
+          <dl>
+            <dt>Tags</dt>
+            <dd>Coding</dd>
+            <dt>Technologies</dt>
+            <dd>Wordpress, Firebase</dd>
+            <dt>Categories</dt>
+            <dd>Front End Development</dd>
+            <dt>Project Demo</dt>
+            <dd>
+              <a
+                href="https://www.greatofficespaces.net/commercial-spaces-for-lease/"
+                target="_blank"
+              >
+                Wordpress Rest Api Search Buildings on leasewarehouses
+              </a>
+            </dd>
+            <dt>Repository</dt>
+            <dd>
+              <a
+                href="https://github.com/KravMaguy/Wordpress-Firebase-Api"
+                target="_blank"
+              >
+                Wordpress/Firebase Rest Api Repository
+              </a>
+            </dd>
+            <dt>Buisness Solution</dt>
+            <dd>
+              <p>
+                Give a potential tenant an interface to quickly search through
+                all buildings listed in the database of another website.
+              </p>
+            </dd>
+            <dt>Project Description</dt>
+            <dd>
+              <p>
+                Makes a GET request to firebase database, and outputs all the
+                availabilites on a wordpress website in a table, has search and
+                filtering capabilites. Users can search buildings by name or
+                description, or units by name, price, or size.
+              </p>
+            </dd>
+          </dl>
+
+          <span className="image main">
+            <img
               src="/static/images/codepen_demo_greensock.jpg"
               alt="Codepen.io demo of using greensock to turn on a lightbulb"
             />
@@ -218,10 +437,58 @@ class Main extends React.Component {
             </dd>
             <dt>Improvements</dt>
             <dd>
-              Maybe adjust the timing of the flickering, refactor the code.
+              Perhaps adjust the timing of the flickering, refactor the code.
               Overall it looks like a scary movie, and the dusty warehouse adds
-              to the effect. Perhaps add a mouse scuffling across the floor for
-              humor.
+              to the effect. Add a mouse scuffling across the floor for humor.
+            </dd>
+          </dl>
+
+          <span className="image main">
+            <img
+              src="/static/images/3d-floorplan.jpg"
+              alt="3D floor plans buisness directory"
+            />
+          </span>
+          <h2>3D floor plans buisness directory.</h2>
+          <dl>
+            <dt>Tags</dt>
+            <dd>Coding, Design</dd>
+
+            <dt>Technologies</dt>
+            <dd>Animation Template, CSS, Javascript, Code Drops</dd>
+
+            <dt>Categories</dt>
+            <dd>Front End Development</dd>
+
+            <dt>Live Link</dt>
+            <dd>
+              <a
+                href="https://www.greatofficespaces.net/office-warehouse-for-lease/"
+                target="_blank"
+              >
+                3D Floor plans of spaces and buisnesses in a commercial building
+              </a>
+            </dd>
+            <dt>Original Article</dt>
+            <dd>
+              <a
+                href="https://tympanus.net/codrops/2016/04/13/interactive-3d-mall-map/"
+                target="_blank"
+              >
+                Interactive 3D Mall Map
+              </a>
+            </dd>
+
+            <dt>Project Description</dt>
+            <dd>
+              <p>
+                Display an office building lay out in the form of a 3D map, and
+                layered Spaceplans, a user can search for a buisness in the
+                building using the search form at the top. As well as go through
+                the different levels of the building for a visual
+                interpretation. Click anywhere on the intro screen to skip the
+                introductory animation and go to the space plans.
+              </p>
             </dd>
           </dl>
 
@@ -237,8 +504,8 @@ class Main extends React.Component {
         >
           <h2 className="major">About</h2>
           <p>
-            Abe enjoys studying foreign languages, travel, going to the gym, and
-            martial arts, he currently resides in Chicago.
+            Abe enjoys studying foreign languages, travel, and teaching martial
+            arts.
           </p>
           {close}
         </article>
